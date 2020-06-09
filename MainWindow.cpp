975a1434489ed5b2d0da982bcd91f7f9e4da6d83
sv.yarakaev <yarakaeff@gmail.com>
2020-06-09 12:55:53 +0300

    add clicked signals. Need alone memeber func for write to tyhe log

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,36 +6,48 @@
 MainWindow::MainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade)
         : Gtk::Window(cobject),
           m_refGlade(refGlade),
-          m_pButton(nullptr)
+          m_pButton(nullptr),
+          m_pComboBoxIPAddr(nullptr),
+          m_pViewLog(nullptr),
+          m_pbtnLoad(nullptr),
+          iter(nullptr)
 {
+    set_title("Proto for loader MPC-L");
     //Get the Glade-instantiated Button, and connect a signal handler:
     m_refGlade->get_widget("btnQuit", m_pButton);
     if(m_pButton)
     {
         m_pButton->signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::on_button_quit) );
     }
+    m_refGlade->get_widget("comboIPAddr", m_pComboBoxIPAddr);
+    m_refGlade->get_widget("viewLog", m_pViewLog);
+    m_refGlade->get_widget("btnLoad", m_pbtnLoad);
+
+    m_pbtnLoad->signal_clicked().connect(sigc::mem_fun(*this, &MainWindow::on_button_Load));
+
+    m_refTextBuffer1 = Gtk::TextBuffer::create();
+    iter = m_refTextBuffer1->get_iter_at_offset(0);
+
+    iter = m_refTextBuffer1->insert (iter, "Create and running interface for load\n");
+    //iter = m_refTextBuffer1->get_iter_at_offset(0);
+    m_pViewLog->set_buffer(m_refTextBuffer1);
+
 }
 
-// The first two parameters are mandatory in a constructor that will be called
-// from Gtk::Builder::get_widget_derived().
-// Additional parameters, if any, correspond to additional arguments in the call
-// to Gtk::Builder::get_widget_derived().
-MainWindow::MainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade,
-                       bool is_glad)
-        : MainWindow(cobject, refGlade) // Delegate to the other constructor
-{
-    // Show an icon.
-    auto pImage = Gtk::make_managed<Gtk::Image>();
-    pImage->set_from_icon_name(is_glad ? "face-smile" : "face-sad", Gtk::ICON_SIZE_DIALOG);
-    pImage->show_all();
-    //get_content_area()->pack_start(*pImage);
-}
 
 MainWindow::~MainWindow()
 {
 }
 
 void MainWindow::on_button_quit()
+
 {
+    iter = m_refTextBuffer1 ->insert(iter,"Bye!\n");
+    m_pViewLog->set_buffer(m_refTextBuffer1);
     hide(); //hide() will cause Gtk::Application::run() to end.
+}
+
+void MainWindow::on_button_Load() {
+    iter = m_refTextBuffer1 ->insert(iter,"IP address button load click\n");
+    m_pViewLog->set_buffer(m_refTextBuffer1);
 }
--- MainWindow.h
+++ MainWindow.h
@@ -6,19 +6,25 @@
 #define WINDOWAPP_MAINWINDOW_H
 
 #include <gtkmm.h>
-
+#include <gtkmm/comboboxtext.h>
 
 class MainWindow : public Gtk::Window {
 public:
     MainWindow(BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &refGlade);
-    MainWindow(BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &refGlade, bool is_glad);
+
 
     virtual ~MainWindow();
 protected:
     void on_button_quit();
-
+    void on_button_Load();
     Glib::RefPtr<Gtk::Builder> m_refGlade;
     Gtk::Button *m_pButton;
+    Gtk::ComboBoxText *m_pComboBoxIPAddr;
+    Gtk::TextView *m_pViewLog;
+    Glib::RefPtr<Gtk::TextBuffer> m_refTextBuffer1, m_refTextBuffer2;
+    Gtk::TextBuffer::iterator iter; //find my seek
+
+    Gtk::Button *m_pbtnLoad;
 };
 
 
